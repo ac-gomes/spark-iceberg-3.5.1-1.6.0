56013cc854eb092cd2cc9c62628f7e71698378ca
ac-gomes <acgoms88@gmail.com>
2024-10-11 00:16:00 -0300

    spark-iceberg base

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # iceberg-lakehouse
+ENV SPARK_VERSION=3.5.1
+ENV AWS_SDK_VERSION=1.12.773
+ENV HADOOP_AWS_VERSION=3.3.4
 
 Airflow: http://localhost:8080
 Local: http://localhost:9000
 Spark-iceberg: http://localhost:8900
+Spark-iceberg Jobs: http://localhost:4040/jobs/
 Trino: http://localhost:8889
--- src/postgres/init-database.sh
+++ src/postgres/init-database.sh
@@ -6,7 +6,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER hive WITH ENCRYPTED PASSWORD 'hive';
     GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
 
-    CREATE DATABASE airflow;
-    CREATE USER airflow WITH ENCRYPTED PASSWORD 'airflow';
-    GRANT ALL PRIVILEGES ON DATABASE metastore TO airflow;
 EOSQL
