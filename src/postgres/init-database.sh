22bfffbb6531b48fcc7ae18e6883f9760a7c8e2b
ac-gomes <acgoms88@gmail.com>
2024-10-12 21:24:13 -0300

    doc;spark-iceber;notebooks;postgres

--- src/postgres/init-database.sh
+++ src/postgres/init-database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#!/bin/bash
+
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
---
+++ src/spark/spark-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SPARK_HOME=/opt/spark
+SPARK_CONF_DIR=/opt/spark/conf
+HADOOP_CONF_DIR=/opt/spark/jars
